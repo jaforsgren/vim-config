56840c923de41efb28f31567b2317909f47473bd
Johan Forsgren <jforsgren@rapidimages.se>
2025-12-14 17:09:02 +0100

    feature: disalbe c# format on save

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.cs = {}
+
+      if opts.format_on_save then
+        local original_format_on_save = opts.format_on_save
+        opts.format_on_save = function(bufnr)
+          if vim.bo[bufnr].filetype == "cs" then
+            return nil
+          end
+          if type(original_format_on_save) == "function" then
+            return original_format_on_save(bufnr)
+          end
+          return original_format_on_save
+        end
+      end
+    end,
+  },
+}
